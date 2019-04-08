e8ab59cef9769fdf731083e40e3ad1962db4f934
McCoy Doherty <mccoy@0587390816.wireless.umich.net>
2019-04-08 18:08:20 -0400

    Seemingly somewhat stable again

--- ads/forms.py
+++ ads/forms.py
@@ -22,7 +22,7 @@ class CreateForm(forms.ModelForm):
 
     class Meta:
         model = Ad
-        fields = ['title', 'text', 'picture']  # Picture is manual
+        fields = ['title', 'price', 'text', 'picture']  # Picture is manual
 
     def clean(self) :
         cleaned_data = super().clean()
@@ -37,7 +37,7 @@ class CreateForm(forms.ModelForm):
         # We only need to adjust picture if it is a freshly uploaded file
         f = instance.picture   # Make a copy
         if isinstance(f, InMemoryUploadedFile):  # Extract data from the form to the model
-            bytearr = f.read();
+            bytearr = f.read()
             instance.content_type = f.content_type
             instance.picture = bytearr  # Overwrite with the actual image data
 
--- ads/views.py
+++ ads/views.py
@@ -34,9 +34,13 @@ from ads.models import Ad
 
 from django.views import View
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from ads.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ads.forms import CreateForm
 
 class AdListView(OwnerListView):
     model = Ad
@@ -59,3 +63,41 @@ class AdUpdateView(OwnerUpdateView):
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ad_delete.html"
+
+class AdFormView(LoginRequiredMixin, View):
+    template = 'ads/form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        if not pk : 
+            form = CreateForm()
+        else: 
+            ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(instance=ad)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Adjust the model owner before saving
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+        print("!")
+        return redirect(self.success_url)
+
+def stream_file(request, pk) :
+    ad = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = ad.content_type
+    response['Content-Length'] = len(ad.picture)
+    response.write(ad.picture)
+    return response
